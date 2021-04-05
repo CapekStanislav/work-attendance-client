cf2053201491d3c5ca0e778b79760a148d19c1d9
Stanislav Čapek <stanislav.capek@plus4u.net>
2021-04-05 13:15:03 +0200

    fix useIsHoliday calls to many times

--- src/hooks/useIsHoliday.js
+++ src/hooks/useIsHoliday.js
@@ -14,5 +14,7 @@ export default function useIsHoliday(date) {
             .withTemplateParameters({date: dateYMD})
             .follow("existsByDate")
             .getResource().result;
+    }, {
+        staleTime: 300000
     });
 }
