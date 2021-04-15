1a14337a323aac1c624a1af3e7fae1b7b2f21933
Stanislav Čapek <stanislav.capek@plus4u.net>
2021-04-15 20:55:37 +0200

    the color differentiation for holiday and weekday (yellow, grey and green)

--- src/hooks/useWeekHolidayColorSchema.js
+++ src/hooks/useWeekHolidayColorSchema.js
@@ -2,12 +2,28 @@ import useIsWeekday from "./useIsWeekday";
 import useIsHoliday from "./useIsHoliday";
 
 export default function useWeekHolidayColorSchema(date) {
+    const weekdayColor = "secondary";
+    const holidayColor = "warning";
+    const weekendAndHolidayColor = "success"
+    const defaultColor = "light";
 
     const isWeekday = useIsWeekday();
     const {data: isHoliday} = useIsHoliday(date);
-    return {
-        bg: isHoliday ? "success" : isWeekday(date) ? "secondary" : "light",
+
+    const _resolveColor = (isWeekday, isHoliday) => {
+        if (isWeekday && isHoliday) {
+            return weekendAndHolidayColor;
+        } else if(isWeekday) {
+            return  weekdayColor;
+        } else if(isHoliday) {
+            return holidayColor;
+        } else {
+            return defaultColor;
+        }
     }
 
+    return {
+        bg: _resolveColor(isWeekday(date),isHoliday)
+    }
 
 }
